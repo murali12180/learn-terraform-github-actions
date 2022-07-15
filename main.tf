1a33284292a412f10b5e76e40addda314034413a
Murali Pathivada <muralipathivada@gmail.com>
2022-07-15 14:16:14 -0400

    test 2

--- main.tf
+++ main.tf
@@ -191,9 +191,9 @@ resource "aws_ssoadmin_permission_set_inline_policy" "example" {
   permission_set_arn = aws_ssoadmin_permission_set.example.arn
 }
 
-# managed policies 1
+# managed policies 2
 resource "aws_ssoadmin_managed_policy_attachment" "example" {
-  for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess"])
+  for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess","arn:aws:iam::aws:policy/AWSCloudTrailReadOnlyAccess"])
   instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
   managed_policy_arn = each.value
   permission_set_arn = aws_ssoadmin_permission_set.example.arn
