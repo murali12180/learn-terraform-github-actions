6af0ab8188da6221ae6bc63acc90db728197eee9
Murali Pathivada <muralipathivada@gmail.com>
2022-07-15 14:33:58 -0400

    test4

--- main.tf
+++ main.tf
@@ -191,7 +191,22 @@ resource "aws_ssoadmin_permission_set_inline_policy" "example" {
   permission_set_arn = aws_ssoadmin_permission_set.example.arn
 }
 
-# managed policies 3
+
+#data "aws_ssoadmin_instances" "example" {}
+
+#resource "aws_ssoadmin_permission_set" "example" {
+#  name         = "Example"
+#  instance_arn = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+#}
+
+resource "aws_ssoadmin_managed_policy_attachment" "example" {
+  for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess","arn:aws:iam::aws:policy/AWSCloudTrailReadOnlyAccess"])
+  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
+  managed_policy_arn = each.value
+  permission_set_arn = aws_ssoadmin_permission_set.example.arn
+}
+
+# managed policies 4
 #resource "aws_ssoadmin_managed_policy_attachment" "example" {
 #  for_each           = toset(["arn:aws:iam::aws:policy/AWSLambda_FullAccess","arn:aws:iam::aws:policy/AWSCloudTrailReadOnlyAccess"])
 #  instance_arn       = tolist(data.aws_ssoadmin_instances.example.arns)[0]
